61037ad6f9476b14e68824d8366e7beb0d72ce00
Gerson C Filho <gersoncfilho@mac.com>
2025-05-04 12:21:40 +0100

    Enhance localization and UI in WPF ToDo App
    Updated MainWindow.xaml to set title, dimensions, and layout.
    Introduced LocalizationManager for language support and added
    localized string resources in Strings.ger.resx and Strings.resx.
    Updated MainViewModel to support property change notifications.
    Added new image resource, purple-calendar.jpg, for UI enhancement.

---
+++ WpfToDoApplication/Resources/LocalizationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfToDoApplication.Resources
+{
+    public class LocalizationManager
+    {
+        public static void SetLanguage(string cultureCode)
+        {
+            var culture = new CultureInfo(cultureCode);
+            Thread.CurrentThread.CurrentUICulture = culture;
+            Thread.CurrentThread.CurrentCulture = culture;
+        }
+
+        public static string WindowTitle => Strings.WindowTitle;
+    }
+}
---
+++ WpfToDoApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfToDoApplication.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _title = "WPF Todo Application1";
+        private string _description = "A simple WPF application to manage your tasks.";
+        private string _language = "en-US";
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    OnPropertyChanged(nameof(Title));
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    OnPropertyChanged(nameof(Description));
+                }
+            }
+        }
+
+        public string Language
+        {
+            get { return _language; }
+            set
+            {
+                if (_language != value)
+                {
+                    _language = value;
+                    OnPropertyChanged(nameof(Language));
+                }
+            }
+        }
+    }
+}
